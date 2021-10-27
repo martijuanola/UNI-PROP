8e5ce35676e3e2c412ceea5a560546419c8008f4
jaume.casals.vilaplana <jaume.casals.vilaplana@est.fib.upc.edu>
2021-10-27 09:21:11 -0700

    classes i funcions basiques

--- src/recomanador/domini/controladorDomini.java
+++ src/recomanador/domini/controladorDomini.java
@@ -2,5 +2,10 @@ package src.recomanador.domini;
 import src.recomanador.persistencia.*;
 
 public class controladorDomini {
-    
+    controladorPersistencia cp;
+    conjuntUsuaris cu;
+    public controladorDomini() {
+        cp = new controladorPersistencia();
+        cu = new conjuntUsuaris();
+    }
 }
