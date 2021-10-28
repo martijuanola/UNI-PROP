926e14fc6bf6bd220ae9149f760dc8dc3742313a
marti.juanola <marti.juanola@est.fib.upc.edu>
2021-10-28 11:11:10 +0200

    faltava cjvaloracions

--- src/recomanador/domini/conjuntValoracions.java
+++ src/recomanador/domini/conjuntValoracions.java
@@ -2,6 +2,6 @@ package src.recomanador.domini;
 
 import java.util.ArrayList;
 
-public class conjuntValoracions extends ArrayList<valoracio> {
+public class ConjuntValoracions extends ArrayList<Valoracio> {
     
 }
