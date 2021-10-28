e2e3b8f52d7cffd757c3039514efaf5ea87bd117
Pol Sturlese <pol.sturlese@estudiantat.upc.edu>
2021-10-28 11:19:35 +0200

    rename

---
+++ src/recomanador/domini/ControladorDomini.java
@@ -0,0 +1,30 @@
+package src.recomanador.domini;
+import src.recomanador.persistencia.*;
+
+public class ControladorDomini {
+	
+	/*----- CONSTANT -----*/
+	public static final int NULL_ID = -1;
+	
+	/*----- ATRIBUTS -----*/
+	
+    ControladorPersistencia cp;
+    ConjuntUsuaris cu;
+    int id;
+    
+    /*----- CONSTRUCTORS -----*/
+    public ControladorDomini() {
+        cp = new ControladorPersistencia();
+        cu = new ConjuntUsuaris();
+        id = NULL_ID;
+    }
+    
+    /*----- ALTRES -----*/
+    
+    //Pre: identificador és vàlida (i.e. identificador és diferent a NULL_ID)
+    //Post: s'iguala l'id de la classe a identificador
+    public void establir_id_activa(int identificador)
+    {
+		id = identificador;
+	}
+}
