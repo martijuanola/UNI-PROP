b624e2dda5642d14178daca297c3d8d14e4ada13
pol.sturlese <pol.sturlese@est.fib.upc.edu>
2021-11-11 17:49:18 +0100

    afegir load + idees persistència

---
+++ src/recomanador/persistencia/ControladorLoad.java
@@ -0,0 +1,20 @@
+package src.recomanador.persistencia;
+
+import java.io.File;
+import java.io.File;
+import java.util.ArrayList;
+
+public class ControladorLoad {
+        
+    public ControladorLoad()
+    {
+	}
+    
+    //estructura de prova per a obrir la carpeta
+    public ArrayList<ArrayList<String>> carregarUsuaris(File file)
+    {
+		ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>;
+		FileReader f = new FileReader(file);
+		return null;
+	}
+}
--- src/recomanador/persistencia/ControladorPersistencia.java
+++ src/recomanador/persistencia/ControladorPersistencia.java
@@ -1,6 +1,30 @@
 package src.recomanador.persistencia;
 
+import java.io.File;
 
 public class ControladorPersistencia {
+    File carpeta;
+    File f;
     
+    public ControladorPersistencia()
+    {
+		carpeta = null;
+		f = null;
+	}
+    
+    //estructura de prova per a obrir la carpeta
+    public boolean escollirProjecte(String s)
+    {
+		try
+		{
+			carpeta = new File(s);
+		}
+		catch(NullPointerException n)
+		{
+			return false;
+		}
+		//generar els fitxers f a partir de la carpeta
+		f = new File(carpeta, "items");
+		return true;
+	}
 }
