a664dbbb406ab865ebdda1da43e5ed1611ad842c
jaume.casals.vilaplana <jaume.casals.vilaplana@est.fib.upc.edu>
2021-11-14 08:09:50 -0800

    Documentar Item

--- src/recomanador/domini/Item.java
+++ src/recomanador/domini/Item.java
@@ -6,14 +6,23 @@ public class Item implements Comparable<Item>{
 
     private ArrayList<ArrayList<String>> atributs; //Vector on cada atribut té un vector. Pot ser buit
 
-
+    /**
+     * Constructor buit
+     */
     public Item() {
     }
 
+    /**
+     * Es crea l'item i s'assigna els atributs a l'item
+     * @param atributs llista d'atributs d'items on cada atribut pot tenir diversos atributs
+     */
     public Item(ArrayList<ArrayList<String>> atributs) {
         this.atributs = atributs;
     }
 
+    /**
+     * Imprimeix els atributs
+     */
     public void print() {
         for (int i = 0; i < atributs.size(); ++i) {
             int g = atributs.get(i).size();
@@ -28,15 +37,32 @@ public class Item implements Comparable<Item>{
         System.out.println("");
     }
 
+    /**
+     * Retorna l'atribut de la posició i
+     * @param i index del atribut: entro 0 i el nombre d'atributs
+     * @return llista dels sub atributs
+     */
     public ArrayList<String> getAtribut(int i) {
         return atributs.get(i);
     }
 
+    /**
+     * Retorna l'atribut de la posició id de ConjuntItems
+     * @return llista del atribut id (Només hi hauria d'haver un atribut, però no està assegurat)
+     */
     public ArrayList<String> getID() {
         return atributs.get(ConjuntItems.id);
     }
 
     @Override
+    /**
+     * Funció que fa override al compareTo per defecte, compara 2 items per els seus id's
+     * @param otherItem L'altre item que compara
+     * @return Retorna un nombre depenent de la comparació entre l'item al que es fa la crida de la funció i el paràmetre
+     * 1 si implícit > otherItem, 
+     * 0 si implícit > otherItem, 
+     * -1 si implícit < otherItem
+     */
     public int compareTo(Item otherItem) {
         ArrayList<String> id1 = getID();
         ArrayList<String> id2 = otherItem.getID();
