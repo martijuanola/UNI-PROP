59ccc2950dd634606bd26ae4e016f2fcee7f6082
adri <adria.fernandez.clemente@estudiantat.fib.upc.edu>
2021-11-16 00:20:25 +0100

    arreglat commit anterior, ups

--- src/drivers/DriverItemValoracioEstimada.java
+++ src/drivers/DriverItemValoracioEstimada.java
@@ -18,11 +18,11 @@ public class DriverItemValoracioEstimada {
     public static void main(String[] args) {
         ArrayList<ItemValoracioEstimada> items = new ArrayList<ItemValoracioEstimada>();
 
-        items.add(new ItemValoracioEstimada(2.5f, "itemA"));
-        items.add(new ItemValoracioEstimada(5.5f, "itemB"));
-        items.add(new ItemValoracioEstimada(0.5f, "itemC"));
-        items.add(new ItemValoracioEstimada(7.5f, "itemD"));
-        items.add(new ItemValoracioEstimada(1.5f, "itemE"));
+        //items.add(new ItemValoracioEstimada(2.5f, "itemA"));
+        //items.add(new ItemValoracioEstimada(5.5f, "itemB"));
+        //items.add(new ItemValoracioEstimada(0.5f, "itemC"));
+        //items.add(new ItemValoracioEstimada(7.5f, "itemD"));
+        //items.add(new ItemValoracioEstimada(1.5f, "itemE"));
 
         Collections.sort(items);
 
