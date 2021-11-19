6d4189a54df4f3e01c9cfaf5f53d08960d537db4
jaume.casals.vilaplana <jaume.casals.vilaplana@est.fib.upc.edu>
2021-11-19 07:41:50 -0800

    Driver UnionIntersection acabat

--- src/drivers/DriverUnionIntersection.java
+++ src/drivers/DriverUnionIntersection.java
@@ -7,22 +7,9 @@ import src.recomanador.domini.Utils.UnionIntersection;
 
 public class DriverUnionIntersection {
 	static private Scanner scanner;
-    
-    static private UnionIntersection c;
-
-    static private boolean class_initalised;
-    //static private Classe1 c1;
-    //static private Classe2 c2;
-    static private String s1;
-    static private String s2;
-    static private boolean b;
-    static private int n;
-    static private int m;
-    static private float f;
-    
+        
     public static void main(String[] args) {
    		scanner = new Scanner(System.in);
-		class_initalised = false;
 		
 		String s = "Options: \n" +
 		"-1. exit\n" +
@@ -60,19 +47,11 @@ public class DriverUnionIntersection {
     
     static private void mostra_1() {
 		System.out.println("Testing function getUnion");
+
 		//demanar l'input
-		
 		System.out.println("Enter the size of the first array:");
-		int n1 = -1;
-		while (n1 == -1) {
-            try {
-                int aux = scanner.nextInt();
-                n1 = aux;
-            }
-            catch (Exception e) {
-                System.out.println("Enter an Integer for the size of the first array");
-            }
-		}
+		int n1 = scanner.nextInt();
+
         System.out.println("Enter " + n1 + " strings in diferent lines:");
 		ArrayList<String> l1 = new ArrayList<String>();
 		scanner.nextLine();
@@ -88,19 +67,44 @@ public class DriverUnionIntersection {
 		for (int i = 0; i < n2; ++i) {
             l2.add(scanner.nextLine());
 		}
+
 		//executar la funcionalitat
 		ArrayList<String> result = new ArrayList<String>();
 		result = UnionIntersection.getUnion(l1, l2);
+
 		//mostrar output
 		System.out.println("Union: ");
         System.out.println(result);
 	}
     static private void mostra_2() {
 		System.out.println("Testing function getIntersection");
+
 		//demanar l'input
+		System.out.println("Enter the size of the first array:");
+		int n1 = scanner.nextInt();
+
+        System.out.println("Enter " + n1 + " strings in diferent lines:");
+		ArrayList<String> l1 = new ArrayList<String>();
+		scanner.nextLine();
+		for (int i = 0; i < n1; ++i) {
+            l1.add(scanner.nextLine());
+		}
 		
+		System.out.println("Enter the size of the second array:");
+		int n2 = scanner.nextInt();
+        System.out.println("Enter " + n2 + " strings in diferent lines:");
+		ArrayList<String> l2 = new ArrayList<String>();
+		scanner.nextLine();
+		for (int i = 0; i < n2; ++i) {
+            l2.add(scanner.nextLine());
+		}
+
 		//executar la funcionalitat
-		
+		ArrayList<String> result = new ArrayList<String>();
+		result = UnionIntersection.getIntersection(l1, l2);
+
 		//mostrar output
+		System.out.println("Intersection: ");
+        System.out.println(result);
 	}
 }
