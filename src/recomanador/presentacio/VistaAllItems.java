5403c37efd77dee4f501fe7879241a9702220333
jaume.casals.vilaplana <jaume.casals.vilaplana@est.fib.upc.edu>
2021-12-20 13:53:11 -0800

    arreglat fallo de linia buida al guardar items

--- src/recomanador/presentacio/VistaAllItems.java
+++ src/recomanador/presentacio/VistaAllItems.java
@@ -15,6 +15,10 @@ public class VistaAllItems extends JFrame {
     
     JPanel panel;
 
+    JButton exit;
+
+    VistaAllItems instancia;
+
     //vs és la vistaSessioNova que ha creat aquesta vista
     public VistaAllItems(ArrayList<String> nomAtributs, ArrayList<String> tipusAtributs, VistaSessioNova vsn) {
         vs = vsn;
@@ -33,6 +37,7 @@ public class VistaAllItems extends JFrame {
     }
 
     private void crearVistaItems() {
+        instancia = this;
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) { //Quan es tanca la finestra es crida aquesta funció
@@ -42,14 +47,15 @@ public class VistaAllItems extends JFrame {
         });
         panel = new JPanel();
         panel.setLayout(new GridLayout(1, 1));
-        JButton b = new JButton("hola");
-        b.addActionListener(new ActionListener() {
+
+        exit = new JButton("Guardar i tornar");
+        exit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e)
             {
-                System.exit(0);
+                dispatchEvent(new WindowEvent(instancia, WindowEvent.WINDOW_CLOSING));
             }
         });
-        panel.add(b);
+        panel.add(exit);
         
 
         JScrollPane scrollFrame = new JScrollPane(panel);
