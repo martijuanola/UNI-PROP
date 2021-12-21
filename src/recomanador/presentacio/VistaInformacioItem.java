278de8c29b74772fb60eb7b91c8374c5aeaef458
jaume.casals.vilaplana <jaume.casals.vilaplana@est.fib.upc.edu>
2021-12-21 09:49:22 -0800

    push amb info items a la mitat per fer merge

--- src/recomanador/presentacio/VistaInformacioItem.java
+++ src/recomanador/presentacio/VistaInformacioItem.java
@@ -1,18 +1,39 @@
 package src.recomanador.presentacio;
-
-import java.awt.*;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class VistaInformacioItem extends JFrame {
+
+    VistaAllItems vi;
+    ArrayList<ArrayList<String>> it;
+
+    boolean allI; //Indica si ve de all items (true) o de vistausuari (false)
+
     public VistaInformacioItem(ArrayList<ArrayList<String>> item, VistaAllItems inst) {
-        
+        vi = inst;
+        it = item;
+        allI = true;
+        crearVistaInfoItem();
+    }
+
+    //Vista informacio item cridada per VistaUsuari
+    public VistaInformacioItem(ArrayList<ArrayList<String>> item) {
+        it = item;
+        allI = false;
+        crearVistaInfoItem();
+    }
+
+    private void crearVistaInfoItem() {
+        setLayout(new BorderLayout());
+
+        JPanel boto = new JPanel();
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Escull el tipus dels atributs");
         pack();
         setMinimumSize(new Dimension(getBounds().getSize().width, 200));
         setVisible(true);
-        inst.infoFi();
     }
 }
