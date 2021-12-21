c6139f749677c3fb1501627b722a46f2a6ede830
pol.sturlese <pol.sturlese@est.fib.upc.edu>
2021-12-21 20:50:23 +0100

    treueq q test

--- src/recomanador/presentacio/VistaTestAlgorisme.java
+++ src/recomanador/presentacio/VistaTestAlgorisme.java
@@ -56,7 +56,6 @@ public class VistaTestAlgorisme extends JFrame {
     
     int original_alg;
     int original_k;
-    int original_q;
     ArrayList<String> original_pesos;
     
     /*----- FUNCIONS -----*/
@@ -70,7 +69,6 @@ public class VistaTestAlgorisme extends JFrame {
         
         original_alg = Integer.parseInt(cp.getAlgorisme());
         original_k = Integer.parseInt(cp.getK());
-        original_q = Integer.parseInt(cp.getQ());
         try
 		{
 			original_pesos = cp.getPesos();
@@ -85,14 +83,12 @@ public class VistaTestAlgorisme extends JFrame {
 					"apartat són temporals. Es revertiran en sortir.");
 			alg_lab = new JLabel("Algorisme seleccionat: ");
 			k_param_lab = new JLabel("Paràmetre K:");
-			q_param_lab = new JLabel("Paràmetre Q:");
 			
 			alg = new JComboBox(algorismes);
 			alg.setSelectedIndex(original_alg);
 			
-			int nb = Math.max(original_q, original_k);
+			int nb = Math.max(1, original_k);
 			nb = Math.max(cp.getAllItems().size(), nb);
-			nb = Math.max(1, nb);
 			
 			k = new JSlider(JSlider.HORIZONTAL, 1, nb, 5);
 			k.setPaintTicks(true);
@@ -101,29 +97,17 @@ public class VistaTestAlgorisme extends JFrame {
 			k.setValue(original_k);
 			
 			ks = new JSpinner(new SpinnerNumberModel(original_k, 1, nb, 1));
-			
-			q = new JSlider(JSlider.HORIZONTAL, 1, nb, 5);
-			q.setPaintTicks(true);
-			q.setPaintLabels(true);
-			q.setMajorTickSpacing(nb/5);
-			q.setValue(original_q);
-			
-			qs = new JSpinner(new SpinnerNumberModel(original_q, 1, nb, 1));
-			
+						
 			tornar_enrere = new JButton("Enrere");
 			
-			
 			dataPanel = new JPanel();
-			dataPanel.setLayout(new GridLayout(3, 3));     
+			dataPanel.setLayout(new GridLayout(2, 3));     
 			dataPanel.add(alg_lab);
 			dataPanel.add(alg);
 			dataPanel.add(new JLabel(""));
 			dataPanel.add(k_param_lab);
 			dataPanel.add(k);
 			dataPanel.add(ks);
-			dataPanel.add(q_param_lab);
-			dataPanel.add(q);
-			dataPanel.add(qs);
 			
 			pesos = new JButton("Modificar Pesos");
 			
@@ -194,35 +178,7 @@ public class VistaTestAlgorisme extends JFrame {
 				}
 			}
 		});
-        
-        qs.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
-				if (!spinnerChanged)
-				{
-					spinnerChanged = true;
-					
-					cp.setQ(qs.getValue().toString());
-					q.setValue(Integer.parseInt(qs.getValue().toString()));
-					
-					spinnerChanged = false;
-				}
-			}
-		});
-		
-		q.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
-				if (!barChanged)
-				{
-					barChanged = true;
-					
-					cp.setQ(Integer.toString(q.getValue()));
-					qs.setValue(q.getValue());
-					
-					barChanged = false;
-				}
-			}
-		});
-        
+                
         alg.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent  e) {
 				String algValue = Integer.toString(alg.getSelectedIndex());
@@ -235,7 +191,6 @@ public class VistaTestAlgorisme extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				cp.setAlgorisme(Integer.toString(original_alg));
 				cp.setK(Integer.toString(original_k));
-				cp.setQ(Integer.toString(original_q));
 				try
 				{
 					cp.setPesos(original_pesos);
